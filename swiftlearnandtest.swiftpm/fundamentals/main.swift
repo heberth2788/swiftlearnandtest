98105318971256f0448043629eb4bb64df03add5
Heberth Deza <heberth2788@gmail.com>
2025-03-17 10:09:16 -0500

    print hello world message to screen

---
+++ swiftlearnandtest.swiftpm/fundamentals/main.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  swiftlearnandtest
+//
+//  Created by Heberth Deza on 17/03/25.
+//
+
+import Foundation
+
+sayHello()
+
+func sayHello() {
+    let greeting: String = "Hello, World! By HD."
+    print(greeting)
+}
